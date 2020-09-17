c90e569a2d8528d2d8608c42921f66e0f9c41aa4
yahm23 <yahmed1996a@gmail.com>
2020-09-17 14:31:01 +0100

    successfully added dark mode switch on app, not integrated with ios default dark mode

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {View, StyleSheet, Image, Platform} from 'react-native';
+import React,{useState} from 'react';
+import {View, StyleSheet, Image,Switch, Platform} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -12,9 +12,56 @@ import Account from './components/Account';
 const Tab = createBottomTabNavigator();
 
 const App = () => {
+  const [darkMode, setdarkMode] = useState(false);
+  const toggleSwitch = () => setdarkMode(previousState => !previousState);
 
+  
+  const TabBarIcon = (name,component,imageIndex) => {
+    const images = [
+      require('./icons/home.png'),
+      require('./icons/list.png'),
+      require('./icons/star.png'),
+      require('./icons/user.png'),
+    ]
+    const Component = component;
+    return(
+      <Tab.Screen name={name} 
+        children={()=><Component darkMode={darkMode}/>}
+        options={{ tabBarIcon: ({size,color}) => {
+          
+        return ( <Image 
+          style={{ 
+            width: size,
+            height: size,
+            tintColor:color,
+          }}
+          source={images[imageIndex]} /> );
+      }}}/>
+    )
+  }
+
+  const styles = StyleSheet.create({
+    container:{
+      backgroundColor: darkMode?'#2f2f2f':'white',
+  
+      flex:1,
+      paddingTop: Platform.OS === 'ios' ? 50 : 0,
+    },
+    tabs:{
+      paddingTop:150,
+    }
+  })
+  
+  
   return (
     <View style={styles.container}>
+      <Switch
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={darkMode ? "#fafafa" : "#f4f3f4"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={darkMode}
+      />
       <NavigationContainer >
         <Tab.Navigator tabBarOptions={TabBarStyles}>
             {TabBarIcon("Home",HomeScreen,0)}
@@ -27,45 +74,18 @@ const App = () => {
   )
 }
 
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    paddingTop: Platform.OS === 'ios' ? 50 : 0,
-  },
-  tabs:{
-    paddingTop:150,
-  }
-})
 
-const TabBarIcon = (name,component,imageIndex) => {
-  const images = [
-    require('./icons/home.png'),
-    require('./icons/list.png'),
-    require('./icons/star.png'),
-    require('./icons/user.png'),
-  ]
-  return(
-    <Tab.Screen name={name}
-     component={component} options={{ tabBarIcon: ({size,color}) => {
-      return ( <Image 
-        style={{ 
-          width: size,
-          height: size,
-          tintColor:color
-        }}
-        source={images[imageIndex]} /> );
-    }}}/>
-  )
-}
 
 const TabBarStyles ={
   tabStyle: {
     paddingTop: 20,
   },
+  keyboardHidesTabBar: true,
   style: {
     height: Platform.OS === 'ios' ? 100 : 75,
   },
   labelPosition: 'below-icon',
+  // style:{backgroundColor:"f3f3f3"},
   labelStyle: {
     marginTop: 5,
     marginBottom: 5,
--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,41 +1,85 @@
 import React, {useState} from 'react';
-import {View, Text, Platform, StyleSheet} from 'react-native';
+import {View, Image, Platform, StyleSheet} from 'react-native';
+// import { Card } from 'react-native-elements'
 
-const HomeScreen = () => {
+import { TextInput } from 'react-native';
+
+const HomeScreen = ({darkMode}) => {
+  const[inputText,setInput] = useState('Search for something');
+  const[focused,setFocus] = useState(false);
+
+  const onFocus = () => setFocus(true)
+
+  const styles = StyleSheet.create({
+    home:{
+      flex:1,
+      backgroundColor: darkMode?'#2f2f2f':'white',
+    },
+    inputContainer:{
+      display:"flex",
+      flexDirection:"row",
+      justifyContent:"space-between",
+      borderColor: darkMode?'white':'red',
+      borderRadius:25,
+      borderWidth: 1,
+      alignItems:"center",
+      
+      backgroundColor:darkMode?(focused? 'white':"#2f2f2f"):(focused? 'white':"#fafafa"),
+      
+      margin:25,
+    },
+
+    icon:{
+      height:20,
+      width:20,
+      marginRight:10,
+      marginVertical:10,
+      marginLeft:15,
+      tintColor: darkMode?'white':'red'
+    },
+
+    
+    input:{ 
+      height: 40,
+      color:darkMode?'white':'grey',
+      flex:1,
+      paddingRight:25,
+    },
+
+    text:{
+      color:'black',
+      fontSize:26,
+      textAlign:"center",
+    },
+
+    img:{
+      margin:30,
+      width:100,
+      height:100,
+      justifyContent:"center",
+      borderRadius:100/2
+    }
+
+  })
 
   return (
-    <View style={styles.Home}>
-      <Text style={styles.text}>Welcome to the Home Page, you're on a {Platform.OS === 'ios'? 'IOS':'Android'}</Text>
+    <View style={styles.home}>
+      <View style={styles.inputContainer}>
+      <Image  style={styles.icon} source={require('../icons/search.png')} />
+        <TextInput 
+        onFocus={ () => onFocus() }
+        style={styles.input}
+        clearTextOnFocus={true}
+        onChangeText={text => setInput(text)}
+        value={inputText}
+        />
+
+      </View>
     </View>
   )
+
+
 }
 
-
-const styles = StyleSheet.create({
-
-  Home:{
-    // height:,
-    // padding:15,
-    backgroundColor:'beige'
-  },
-
-  text:{
-    color:'black',
-    fontSize:26,
-    textAlign:"center",
-    // display:'flex',
-    // flexShrink:1
-  },
-
-  img:{
-    margin:30,
-    width:100,
-    height:100,
-    // textAlign:"center",
-    justifyContent:"center",
-    borderRadius:100/2
-  }
-
-})
 
 export default HomeScreen;  
